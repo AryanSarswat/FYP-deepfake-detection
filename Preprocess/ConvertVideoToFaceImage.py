b2e04735ad2b5786a752798992d0966d83fe82da
Aryan Sarswat <65006917+AryanSarswat@users.noreply.github.com>
2023-02-23 10:28:52 +0800

    Multiproccessing attempt

--- Preprocess/ConvertVideoToFaceImage.py
+++ Preprocess/ConvertVideoToFaceImage.py
@@ -1,106 +1,96 @@
-import gc
 import os
+from os import cpu_count
+from pathlib import Path
+
+import numpy as np
+
+os.environ["MKL_NUM_THREADS"] = "1"
+os.environ["NUMEXPR_NUM_THREADS"] = "1"
+os.environ["OMP_NUM_THREADS"] = "1"
+from functools import partial
+from glob import glob
+from multiprocessing.pool import Pool
 
 import cv2
-import numpy as np
-import pandas as pd
-import torch
+import torch    
 from facenet_pytorch import MTCNN
-from torch.utils.data import DataLoader, Dataset
+
+cv2.ocl.setUseOpenCL(False)
+cv2.setNumThreads(0)
 from tqdm import tqdm
-
-# Optimisations
-torch.backends.cudnn.benchmark = True
-torch.backends.cudnn.enabled = True
-torch.backends.cuda.matmul.allow_tf32 = True
-torch.backends.cudnn.allow_tf32 = True
-
-#! Hyper parameters
-# Debugging
-torch.autograd.set_detect_anomaly(False)
-torch.autograd.profiler.profile(False)
-torch.autograd.profiler.emit_nvtx(False)
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 mtcnn = MTCNN(image_size=224, select_larget=True, post_process=False, device=device)
 
+def extract_face(img):
+    img = img.cvtColor(cv2.COLOR_BGR2RGB)
+    img = torch.from_numpy(img.transpose(2, 0, 1)).to(device)
+    boxes, probs = mtcnn(img)
 
-def get_bounding_box(x1, y1, x2, y2):
-    y2 += (y2 - y1) / 10
-    w = x2 - x1
-    h = y2 - y1
-    diff_h_w = (h - w) / 2
-    x1 -= diff_h_w
-    x2 += diff_h_w
-    return x1, y1, x2, y2
-
-def convert_video_to_images(src_path, dest_path, num_frames=32):
-    vidcap = cv2.VideoCapture(src_path)
+    if len(boxes) == 0:
+        return None
+    else:
+        xmin, ymin, xmax, ymax = boxes[0]
+        x_diff = xmax - xmin
+        y_diff = ymax - ymin
+        
+        x_min = int(xmin - 0.3 * x_diff)
+        x_max = int(xmax + 0.3 * x_diff)
+        y_min = int(ymin - 0.3 * y_diff)
+        y_max = int(ymax + 0.3 * y_diff)
+        
+        crop_img = img[y_min:y_max, x_min:x_max]
+        crop_img = crop_img.cpu().numpy().transpose(1, 2, 0)
+        crop_img = cv2.resize(crop_img, (224, 224))
+        
+    return crop_img
     
-    total_frames = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
+def get_video_paths(root_dir):
+    video_paths = []
+    for ext in ["mp4", "avi", "mov"]:
+        video_paths += glob(os.path.join(root_dir, f"*.{ext}"))
+    return video_paths
+
+def extract_video(src_path, dest_path, num_frames=32, isReal=False):
+    capture = cv2.VideoCapture(src_path)
+    total_frames = int(capture.get(cv2.CAP_PROP_FRAME_COUNT))
     
     idxs = np.linspace(0, total_frames, num=num_frames, endpoint=False, dtype=int)
-    
-    dest_path = dest_path.replace('.mp4', '')
+
+    filename = src_path.split(os.pathsep)[-1]
+    filename = filename.split('.')[0]
+    dest_path = os.path.join(dest_path, filename)
     os.makedirs(dest_path, exist_ok=True)
     
     for idx, frame_num in enumerate(idxs):
-        vidcap.set(cv2.CAP_PROP_POS_FRAMES, frame_num)
-        success, image = vidcap.read()
+        capture.set(cv2.CAP_PROP_POS_FRAMES, frame_num)
+        success, image = capture.read()
         
         if not success:
             break
         
-        boxes, probs = mtcnn.detect(image)
-        xmin, ymin, xmax, ymax = boxes[0]
-        xmin, ymin, xmax, ymax = get_bounding_box(xmin, ymin, xmax, ymax)
-        crop_frame = image[int(ymin):int(ymax), int(xmin):int(xmax)]
-        crop_frame = cv2.resize(crop_frame, (224, 224))
-        
+        image = extract_face(image)
+
+        if image is None:
+            break
         
         path_to_save = os.path.join(dest_path, f'{idx}.jpg')
-        cv2.imwrite(path_to_save, crop_frame)
-    
-class ProcessDataset(Dataset):
-    def __init__(self, src_path, dest_path, num_frames, isReal=True):
-        self.X = []
-        for dirpath, dirnames, filenames in tqdm(os.walk(src_path)):
-            for filename in filenames:
-                if filename.endswith('.mp4'):
-                    self.X.append(os.path.join(dirpath, filename))
-        if isReal:
-            dest_path = os.path.join(DEST_PATH, 'real')
-        else:
-            dest_path = os.path.join(DEST_PATH, 'fake')
+        cv2.imwrite(path_to_save, image)
         
-        self.y = dest_path
         
-        if not os.path.exists(dest_path):
-            os.makedirs(dest_path, exist_ok=True)
-        
-        self.num_frames = num_frames
-        
-    def __len__(self):
-        return len(self.X)
-    
-    def __getitem__(self, idx):
-        video_path = self.X[idx]
-        convert_video_to_images(video_path, self.y, num_frames=self.num_frames)
-        return None, None
-
-class DataLoaderWrapper(DataLoader):
-    def __init__(self, src_path, dest_path, isReal, num_frames, workers=8, batch_size=4):
-        dataset = ProcessDataset(src_path=src_path, dest_path=dest_path, num_frames=num_frames, isReal=isReal)
-        super().__init__(dataset, batch_size=batch_size, num_workers=workers)
-
 if __name__ == "__main__":
-    PATH = ''
-    DEST_PATH = 'KoDF/videos_32/'
-    print("[INFO] Preprocessing real videos...")
-    wr = DataLoaderWrapper(PATH, DEST_PATH, isReal=True, num_frames=32)
+    root_dir = '../../../../hdd/data/KoDF/kodf_release/original_videos/'
+    video_paths = get_video_paths(root_dir)
     
-    for n1, n2 in tqdm(wr):
-        temp1, temp2 = n1, n2
-        
-    #print("[INFO] Preprocessing fake videos...")
-    #preprocess(TEST_PATH_1, num_frames=16)
+    dest_dir = Path("KoDF/videos_32")
+    isReal = True
+    
+    if isReal:
+        dest_dir = os.path.join(dest_dir, "real")
+    else:
+        dest_dir = os.path.join(dest_dir, "fake")
+    
+    with Pool(cpu_count()) as pool:
+        with tqdm(total=len(video_paths)) as pbar:
+            for v in pool.imap_unordered(partial(extract_video, dest_path=dest_dir, num_frames=32, isReal=isReal), video_paths):
+                pbar.update()
